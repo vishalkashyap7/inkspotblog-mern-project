01754e3996119f6a4b88dece06fcb41f9e86143c
Vishal Kashyap <vishal.kashyap1@xperi.com>
2024-09-10 15:17:58 +0530

    Loading indicator in home screen

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -1,5 +1,6 @@
 // import { useEffect } from "react";
 
+import { OrbitProgress } from "react-loading-indicators";
 import Post from "../post/Post1";
 import "./posts.css";
 
@@ -8,7 +9,7 @@ export default function Posts({ posts }) {
   return (
     <div className="posts">
       {
-        posts.length === 0 && <p>Oops! It seems you've landed on the last page, but there's no content available here.</p>
+        posts.length === 0 && <div className="loading-div"><OrbitProgress color="red" size="medium" text="" textColor="" /></div>
       }
       {posts.map((p,i) => (
         <Post key={i} post={p} />
